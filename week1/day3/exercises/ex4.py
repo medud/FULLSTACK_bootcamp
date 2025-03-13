63885d1450c77b467185e1a5c488d5280e1de616
medud <simooued1@gmail.com>
2025-03-13 12:13:24 +0000

    ex4

---
+++ week1/day3/exercises/ex4.py
@@ -0,0 +1,61 @@
+class Zoo:
+    def __init__(self, zoo_name):
+        self.name = zoo_name
+        self.animals = []
+
+    def add_animal(self, new_animal):
+        if new_animal not in self.animals:
+            self.animals.append(new_animal)
+            print(f"{new_animal} has been added to the zoo.")
+        else:
+            print(f"{new_animal} is already in the zoo.")
+
+    def get_animals(self):
+        if self.animals:
+            print(f"Animals in {self.name}: {', '.join(self.animals)}")
+        else:
+            print("The zoo has no animals yet.")
+
+    def sell_animal(self, animal_sold):
+        if animal_sold in self.animals:
+            self.animals.remove(animal_sold)
+            print(f"{animal_sold} has been sold.")
+        else:
+            print(f"{animal_sold} is not in the zoo.")
+
+    def sort_animals(self):
+        self.animals.sort()
+        grouped_animals = {}
+        
+        for animal in self.animals:
+            first_letter = animal[0].upper()  
+            if first_letter not in grouped_animals:
+                grouped_animals[first_letter] = []
+            grouped_animals[first_letter].append(animal)
+        
+        return grouped_animals
+
+    def get_groups(self):
+        groups = self.sort_animals()
+        print("Animal groups in the zoo:")
+        for letter, animals in groups.items():
+            print(f"{letter}: {animals}")
+
+new_york_zoo = Zoo("New York Zoo")
+
+new_york_zoo.add_animal("Giraffe")
+new_york_zoo.add_animal("Baboon")
+new_york_zoo.add_animal("Bear")
+new_york_zoo.add_animal("Ape")
+new_york_zoo.add_animal("Cat")
+new_york_zoo.add_animal("Cougar")
+new_york_zoo.add_animal("Eel")
+new_york_zoo.add_animal("Emu")
+
+new_york_zoo.get_animals()
+
+new_york_zoo.sell_animal("Bear")
+
+new_york_zoo.get_animals()
+
+new_york_zoo.get_groups()
