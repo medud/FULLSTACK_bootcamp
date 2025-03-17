97bb23c5b515f8af40dc91e300dbdef530de10bd
medud <simooued1@gmail.com>
2025-03-17 15:30:40 +0000

    dc1

---
+++ week2/day1/dc.sql
@@ -0,0 +1,11 @@
+INSERT INTO actor (first_name, last_name, age)
+VALUES
+    ('med', 'ud', 45),
+    ('zak', 'rd', 35),
+    ('selena', 'gomez', 30);
+
+SELECT COUNT(*) FROM actor;
+
+INSERT INTO actor (first_name, last_name, age)
+VALUES
+	('','','')
